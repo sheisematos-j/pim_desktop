80a61bf546cd84d01cae96ca0c63a21857536135
Sheise Matos <sdjmattos@hotmail.com>
2020-11-04 20:24:10 -0300

    primeiro commit

---
+++ TelaLogin/src/tela/Bitcoin.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tela;
+
+import java.awt.Graphics;
+
+
+/**
+ *
+ * @author sdjma
+ */
+public class Bitcoin {
+
+    static void drawImage(Graphics bitcoin, int i, int i0, int width, int height, login aThis) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
